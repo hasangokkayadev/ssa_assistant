6db83d825362881f97b0f585ea3599d60acaf816
hasangokkayadev <hasangokkayadev@gmail.com>
2025-04-18 21:53:00 +0300

    PDF yüklemesi gerçekleştirildi. Chunking işlemi yazıldı. Embedding ve ChromaDB kodları yazıldı. RAG Tarafı Yazıldı.

--- app.py
+++ app.py
@@ -8,3 +8,18 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains import create_retrieval_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains.combine_documents import create_stuff_documents_chain
+
+load_dotenv()
+
+st.title("RAG Application with Gemini API")
+
+loader = PyPDFLoader("SteelSwarmApocalypse(SSA)Guide.pdf")
+data = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000)
+docs = text_splitter.split_documents(data)
+
+embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+vectorstore = Chroma.from_documents(documents=docs,embedding=embeddings,persist_directory="./chroma_db")
+
+retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs= {"k" : 10})
